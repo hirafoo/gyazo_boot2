6b208d1455e3260e5c02c3f70c850a944aa399c0
hirafoo <hirafoo@cpan.org>
2018-07-28 18:16:51 +0900

    curl and get "index"

---
+++ src/main/java/com/example/gyazo_boot2/RootController.java
@@ -0,0 +1,12 @@
+package com.example.gyazo_boot2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
