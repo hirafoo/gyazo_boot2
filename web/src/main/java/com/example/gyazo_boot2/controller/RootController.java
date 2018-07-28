134664968a6eeb2ce2ed52551c74898966613654
hirafoo <hirafoo@cpan.org>
2018-07-28 21:22:13 +0900

    use Thymeleaf

--- web/src/main/java/com/example/gyazo_boot2/controller/RootController.java
+++ web/src/main/java/com/example/gyazo_boot2/controller/RootController.java
@@ -1,16 +1,19 @@
-package com.example.gyazo_boot2;
+package com.example.gyazo_boot2.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class RootController {
     @Value("${hoge}")
     private String hoge;
 
     @GetMapping("/")
-    public String index() {
-        return "index: hoge is " + hoge;
+    public String index(Model model) {
+        model.addAttribute("hoge", hoge);
+        return "index";
     }
 }
