4f7b0e9a6f6b7efbe75bfa0a288d623b38394d63
hirafoo <hirafoo@cpan.org>
2018-07-28 20:20:14 +0900

    add config and load

--- web/src/main/java/com/example/gyazo_boot2/RootController.java
+++ web/src/main/java/com/example/gyazo_boot2/RootController.java
@@ -1,12 +1,16 @@
 package com.example.gyazo_boot2;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RootController {
+    @Value("${hoge}")
+    private String hoge;
+
     @GetMapping("/")
     public String index() {
-        return "index";
+        return "index: hoge is " + hoge;
     }
 }
