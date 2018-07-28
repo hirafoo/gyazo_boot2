27fc421994da82601aca2c90c05de11bec168837
hirafoo <hirafoo@cpan.org>
2018-07-28 22:39:29 +0900

    rename

--- web/src/test/java/com/example/gyazo_boot2/WebApplicationTests.java
+++ web/src/test/java/com/example/gyazo_boot2/WebApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class GyazoBoot2ApplicationTests {
+public class WebApplicationTests {
 
 	@Test
 	public void contextLoads() {
