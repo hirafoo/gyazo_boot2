2eb531d0688533a5327feca539c50bc6ade51b3e
hirafoo <hirafoo@cpan.org>
2018-08-04 00:17:05 +0900

    entity は config とかに依存しないのでこれでいい

--- common/src/main/java/com/example/gyazo_boot2/entity/Image.java
+++ common/src/main/java/com/example/gyazo_boot2/entity/Image.java
@@ -1,14 +1,9 @@
 package com.example.gyazo_boot2.entity;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Value;
 
 @Data
 public class Image {
-
-    //@Value("${base_url}")
-    //private String baseUrl;
-
     private int    id;
     private String hash;
     private int    createdAt;
